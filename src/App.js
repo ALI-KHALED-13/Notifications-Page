93babb5ea4ad8050d5ac6b16686ed57b73bb5c18
Ali <aliknake@gmail.com>
2023-02-01 15:08:57 +0200

    adding my name

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
       <br />
       <div className="attribution">
         Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank"  rel="noreferrer">Frontend Mentor</a>. 
-        Coded by <a href="#">Your Name Here</a>.
+        Coded by <a href="https://github.com/ALI-KHALED-13">Ali Khaled</a>.
       </div>
     </div>
   );
